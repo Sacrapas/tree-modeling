dd5a95e4140cac5c64e47ed0a501ce1ff2f58a92
Andrey Valikov <avalikov@phoenixit.ru>
2018-04-13 10:28:51 +0300

    [wip] one component way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,65 @@
-import { Component, HostListener } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
 
 
-  public cells: any[] = [
-  {
-    id: 0,
-    name: 'сенсоры',
-    links: [3]
-  },
-  {
-    name: 'мозг',
-    children: [
-      {
-        name: 'память',
-        children: [
-          {
-            name: 'текущая',
-            id: 3,
-            links: [4]
-          },
-          {
-            name: 'долговременная',
-            id: 4,
-            links: [3]
-          }
-        ]
-      },
-      {
-        name: 'таблица приоритетов',
-        links: [5]
-      },
-      {
-        name: 'модуль достижения цели',
-        id: 5,
-        links: [6]
-      }
-    ]
-  },
-  {
-    name: 'кнопки',
-    id: 6
-  }
-];
+  @Input() name: string;
 
-  @HostListener('click')
-  public click(): void {
-    this.cells.push(1);
+  @Input() children: any[];
+
+
+  public cells: any[] = [
+    {
+      id: 0,
+      name: 'сенсоры',
+      links: [3]
+    },
+    {
+      name: 'мозг',
+      children: [
+        {
+          name: 'память',
+          children: [
+            {
+              name: 'текущая',
+              id: 3,
+              links: [4]
+            },
+            {
+              name: 'долговременная',
+              id: 4,
+              links: [3]
+            }
+          ]
+        },
+        {
+          name: 'таблица приоритетов',
+          links: [5]
+        },
+        {
+          name: 'модуль достижения цели',
+          id: 5,
+          links: [6]
+        }
+      ]
+    },
+    {
+      name: 'кнопки',
+      id: 6
+    }
+  ];
+
+  public add(e: Event): void {
+    this.children.push({
+      name:     null,
+      children: null
+    });
+    e.stopPropagation();
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CellComponent } from './cell/cell.component';
-
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, CellComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
